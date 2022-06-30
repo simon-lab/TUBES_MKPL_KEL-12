e726fbc8b09419a93e5f26de8a03df4306f6657b
afrianluthfan <55348072+afrianluthfan@users.noreply.github.com>
2022-06-30 20:32:35 +0700

    asdasd

--- src/Jurnal06/Bentuk.java
+++ src/Jurnal06/Bentuk.java
@@ -18,8 +18,8 @@ public abstract class Bentuk {
     public void setNama(String nama) {//set nama
         this.nama = nama;
     }
-    public String getNama(String kk) {//get nama
-        return nama;
+    public String getNama(String) {//get nama
+        
     }
     public void info() {
         
