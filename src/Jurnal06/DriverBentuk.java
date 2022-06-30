de84bc20719a1e3f6db053c28ca22525244c07be
Raja Simon <rajasimon0@gmail.com>
2022-06-30 21:18:30 +0700

    Update DriverBentuk.java

--- src/Jurnal06/DriverBentuk.java
+++ src/Jurnal06/DriverBentuk.java
@@ -2,13 +2,34 @@ package Jurnal06;
 
 public class DriverBentuk {
     public static void main(String[] args) { //main
-        Kuus K = new Kubus("Kiub", 5);
-        Lngkaran L = new Lingkaran("Sirkel", 14);
-        PersegiPanjang PP = new PersegiPanjang("Rektenggel", 8, 9) {};
+        Kubus K = new Kubus("Kiub", 5);
+        Ligkaran L = new Lingkaran("Sirkel", 14);
+        PesegiPanjang PP = new PersegiPanjang("Rektenggel", 8, 9) {};
         PrismaSegi3 PS3 = new PrismaSegi3("Trainggel Prism", 6, 7, 10);
         L.info();
-        System.out.println("Luas                : "+L.hitungLuas());
+        System.out.println("Luas                : "+L.hitungLus());
         System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        System.out.println("Keliling            : "+L.hitungKeliling());
+        
         PP.info();
         System.out.println("Luas                    : "+L.hitungLuas());
         System.out.println("Keliling                : "+L.hitungKeliling());
@@ -18,6 +39,5 @@ public class DriverBentuk {
         PS3.info();
         System.out.println("Luas Permukaan          : "+PS3.hitungLuasPermukaan());
         System.out.println("Volume                  : "+PS3.hitungVolume());
-        
     }
 }
