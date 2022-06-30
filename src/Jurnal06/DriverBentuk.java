daf3d22ca9d46e98a3b6cfe4a325a51181607930
afrianluthfan <55348072+afrianluthfan@users.noreply.github.com>
2022-06-30 20:45:21 +0700

    Update DriverBentuk.java

--- src/Jurnal06/DriverBentuk.java
+++ src/Jurnal06/DriverBentuk.java
@@ -5,7 +5,7 @@ public class DriverBentuk {
         Kubus K = new Kubus("Kiub", 5);
         Lingkaran L = new Lingkaran("Sirkel", 14);
         PersegiPanjang PP = new PersegiPanjang("Rektenggel", 8, 9) {};
-        PrismaSegi3 PS3 = new PrismaSegi3("Trainggel Prism", 6, 7, 10);
+        PrismaSegi3 PS3 = new PrismaSegi3("Trainggel Prissm", 6, 7, 10);
         L.info();
         System.out.println("Luas                : "+L.hitungLuas());
         System.out.println("Keliling            : "+L.hitungKeliling());
